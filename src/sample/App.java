50df23fa01ade0bebc638a0913bb37a49443ad71
Kanoun Zied <64085047+kanounzied@users.noreply.github.com>
2022-10-26 19:24:36 +0100

    final commit

--- src/sample/App.java
+++ src/sample/App.java
@@ -2,10 +2,14 @@ package sample;
 
 import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
+import com.toedter.calendar.JDateChooser;
+import org.jdatepicker.impl.JDatePickerImpl;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.DateFormatter;
+import javax.swing.text.DefaultFormatterFactory;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,6 +17,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class App {
     private JPanel mainPanel;
@@ -25,13 +31,22 @@ public class App {
     private JLabel qrImage;
     private JButton generateQRButton;
     private JButton saveButton;
+    private JLabel cinAff;
+    private JPanel datePanel;
+    private JFormattedTextField dateDeNaissanceField;
+    JDateChooser dateChooser = new JDateChooser();
 
     public App() {
 
-        final File[] tempQR = {null};
+        dateChooser.setDateFormatString("dd/MM/yyyy");
+        datePanel.add(dateChooser);
+
+        final File[] tempQR = new File[1];
         generateQRButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
                 try {
                     String data = """
                             -----------
@@ -39,6 +54,7 @@ public class App {
                             \nPrenom : """ + prenomField.getText() + """
                             \nCIN : """ + cinField.getText() + """
                             \nNumero Tel :""" + telField.getText() + """
+                            \nDate De Naissance :""" + simpleDateFormat.format(dateChooser.getDate()) + """
                             \nResultat :""" + resultatField.getSelectedItem() + """
                             \n-----------
                             
@@ -52,7 +68,10 @@ public class App {
 //
 //                    ImageIO.write(image, "png", out);
                     qrImage.setIcon(new ImageIcon(bufferedImage));
-
+                    cinAff.setText("CIN : " + cinField.getText());
+                    DateFormat df = SimpleDateFormat.getDateInstance();
+                    DateFormatter dff = new DateFormatter(df);
+                    dateDeNaissanceField.setFormatterFactory(new DefaultFormatterFactory(dff));
                 } catch (WriterException writerException) {
                     writerException.printStackTrace();
                 } catch (IOException ioException) {
@@ -66,23 +85,25 @@ public class App {
             @Override
             public void actionPerformed(ActionEvent e) {
                 BufferedImage image = null;
-                JFileChooser chooser= new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG", "png");
-                chooser.setFileFilter(filter);
-                chooser.addChoosableFileFilter(filter);
+//                JFileChooser chooser= new JFileChooser();
+//                FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG", "png");
+//                chooser.setFileFilter(filter);
+//                chooser.addChoosableFileFilter(filter);
                 try {
 
-                    int choice = chooser.showOpenDialog(null);
-                    if (choice != JFileChooser.APPROVE_OPTION) return;
+//                    int choice = chooser.showOpenDialog(null);
+//                    if (choice != JFileChooser.APPROVE_OPTION) return;
                     image = ImageIO.read(tempQR[0]);
-                    File chosenFile = chooser.getSelectedFile();
-                    String filepath = chosenFile.getAbsolutePath();
-                    chosenFile.delete();
-                    if (!filepath.substring(filepath.lastIndexOf('.') + 1).equals("png")) {
-                        filepath = filepath + ".png";
-                    }
-//                    "C:\\Users\\ziedk\\Desktop\\QRs\\res.png"
-                    File out = new File(filepath);
+//                    File chosenFile = chooser.getSelectedFile();
+//                    String filepath = chosenFile.getAbsolutePath();
+//                    chosenFile.delete();
+//                    if (!filepath.substring(filepath.lastIndexOf('.') + 1).equals("png")) {
+//                        filepath = filepath + ".png";
+//                    }
+                    char separator = File.separatorChar;
+                    String filepath = System.getProperty("user.home") + separator + "Documents" + separator + "QRCodes" + separator;
+                    String filename = cinField.getText() + ".png";
+                    File out = new File(filepath + filename);
                     ImageIO.write(image, "png", out);
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
